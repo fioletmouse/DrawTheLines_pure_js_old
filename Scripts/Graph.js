7f0895e85253da4b2d85bfbbd3d6a8c76104c979
fioletmouse <logika2003@inbox.ru>
2016-06-04 18:59:01 +0600

    Make base lines and settings

---
+++ Scripts/Graph.js
@@ -0,0 +1,52 @@
+﻿var Settings = {
+    x: 0,
+    y:0,
+    backColor: "blueviolet",
+    lineUserLength: 50,
+    lineLength:50,
+    linewidth: 1,
+    lineColor: "blue",
+    dotsCount: 10,
+    step: 5
+}
+
+function changeLineLenghtToDots()
+{
+    var length = parseInt(document.getElementById("length").value);
+    var dots = parseInt(document.getElementById("dotsCount").value);
+
+    Settings.step = Math.floor(length / dots);
+    Settings.lineLength = Settings.step * dots;
+    Settings.dotsCount = dots;
+    Settings.lineUserLength = length;
+}
+
+function startDrawing(e) {
+    Settings.x = e.pageX - canvas.offsetLeft;
+    Settings.y = e.pageY - canvas.offsetTop;
+
+    changeLineLenghtToDots();
+    makeBaseLines(Settings.x, Settings.y, Settings.lineLength);
+    console.log(Settings);
+}
+function makeJoinLines()
+{
+
+}
+function makeBaseLines(x, y, length) {
+
+    DrawLines(x, y, x + length, y);
+    DrawLines(x, y, x - length, y);
+    DrawLines(x, y, x, y + length);
+    DrawLines(x, y, x, y - length);
+}
+
+function DrawLines(fromX, fromY, toX, toY) {
+    context.beginPath();
+    context.lineWidth = Settings.linewidth;
+    context.strokeStyle = Settings.lineColor;
+    context.moveTo(fromX, fromY);
+    context.lineTo(toX, toY);
+    context.stroke();
+    context.closePath();
+}
---
+++ Scripts/Main.js
@@ -0,0 +1,22 @@
+﻿var canvas;
+var context;
+
+window.onload = function () {
+    canvas = document.getElementById("cnv");
+    context = canvas.getContext("2d");
+    
+    // !!! change it using palet
+    canvas.style.backgroundColor = Settings.backColor;
+
+    // update on any window size change.
+    window.addEventListener("resize", redraw);
+    redraw();
+
+    // Подключаем требуемые для рисования события
+    canvas.onclick = startDrawing;
+}
+
+function redraw() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
