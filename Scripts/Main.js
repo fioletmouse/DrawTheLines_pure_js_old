1c493c1d2ad630645ea888b28068e67fc9c8e049
fioletmouse <logika2003@inbox.ru>
2016-06-06 10:41:29 +0600

    Add control panel and libs
    Make background

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -8,6 +8,7 @@ window.onload = function () {
     // !!! change it using palet
     canvas.style.backgroundColor = Settings.backColor;
 
+
     // update on any window size change.
     window.addEventListener("resize", redraw);
     redraw();
@@ -15,7 +16,52 @@ window.onload = function () {
     // Подключаем требуемые для рисования события
     canvas.onclick = startDrawing;
 }
+$(document).ready(function() {
 
+
+   /* $("#showColorPicker").click(function () {
+        if ($("#colorPanel").css("display") == "none") {
+            $("#colorPanel").show();
+        }
+        else {
+            $("#colorPanel").hide();
+        }
+    });*/
+   /* $('#colorpicker').farbtastic('#color');
+    $('[data-toggle="popover"]').popover({
+        //Установление направления отображения popover
+        html: true,
+        placement: 'bottom',
+        trigger: "click",
+        content: function () {
+            return $('#colorPanel');
+        }
+    })*//*.on("click", function () {
+            $("#colorPanel").find("#colorpicker").farbtastic("#color");
+        })*/
+
+    /*$("#color").each(function (i, input) {
+        $this = $(input);
+        return*/ /*$("#color")*/$('[data-toggle="popover"]').popover({
+            title: "Colorpicker <i class='icon-remove pull-right'></i>",
+            trigger: "focus",
+            placement: "bottom",
+            html: true,
+            content: "<div id='colorpicker1'>" + "<div class='color-picker'></div>"+ "</div>"
+        }).on("click", function () {
+            $this = $(this);
+            $target = $("#colorpicker1").find(".color-picker");
+            $target.farbtastic(function (color) {
+                //$this.val(color).css("background-color", color);
+                $("#cnv").css("background-color", color);
+            });
+            //var picker = $.farbtastic($target);
+            //$new.setColor("#e297d7");
+       // });
+    });
+
+
+});
 function redraw() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
